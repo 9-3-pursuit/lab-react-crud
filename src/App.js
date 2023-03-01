2aafe5fc66a93702fc9921acd0bfadb6ecd19319
amber-king <amberking@pursuit.org>
2023-03-01 14:57:37 -0500

    added imports needed to app.js, rough draft

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ import ShowsEditForm from "./components/shows/ShowsEditForm";
 import ShowsIndex from "./components/shows/ShowsIndex";
 import ShowsNewForm from "./components/shows/ShowsNewForm";
 
+// Components added on --v
+import Movies from ".components/movies/Movies"
+import MoviesIndex from ".components/movies/MoviesIndex"
+import MoviesEditForm from "./components/movies/MoviesEditForm"
+import MoviesNewForm from "./components/movies/MoviesNewForm"
+
+
 function App() {
   return (
     <div className="wrapper">
@@ -22,10 +29,10 @@ function App() {
           <Route path="/shows/:id/edit" element={<ShowsEditForm />} />
 
           {/* Created Routes on Own */}
-          <Route path="/movies" element={<MoviesIndex />} />
+          {/* <Route path="/movies" element={<MoviesIndex />} />
           <Route path="/movies/new" element={<MoviesNewForm />} />
           <Route path="/movies/:id" element={<MoviesShowOne />} />
-          <Route path="/movies/:id/edit" element={<MoviesEditForm />} />
+          <Route path="/movies/:id/edit" element={<MoviesEditForm />} /> */}
         </Routes>
         <Footer />
       </Router>
