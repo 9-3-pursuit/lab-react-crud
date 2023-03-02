54c0dc6f030aa54285981d94fa77001b822ab050
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-02 15:06:32 -0500

    Adds remaingin movie CRUD methods

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -46,10 +46,47 @@ export async function updateShow(id, show) {
 }
 
 // Movies
-
+// Index/Get all
 export async function getAllMovies() {
   const apiReponse = await fetch(`${base}/movies`);
   const jsonResponse = await apiReponse.json();
 
   return jsonResponse;
+}
+
+// Show/Get one
+export async function getOneMovie(id) {
+  const apiReponse = await fetch(`${base}/movies/${id}`);
+  const jsonResponse = await apiReponse.json();
+
+  return jsonResponse;
+}
+
+// Create
+export function createMovie(show) {
+  return fetch(`${base}/movies/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", // set the Content-Type header if sending JSON data
+    },
+    body: JSON.stringify(show), // include the data to be sent in the request body
+  });
+}
+
+// Update
+export async function updateMovies(id, show) {
+  return fetch(`${base}/movies/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json", // set the Content-Type header if sending JSON data
+    },
+    body: JSON.stringify(show), // include the data to be sent in the request body
+  });
+}
+
+// Delete
+export function destroyMovie(id) {
+  return fetch(`${base}/movies/${id}`, {
+    method: "DELETE",
+  });
 }
