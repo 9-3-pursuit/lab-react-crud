159a98f4703e3a41753760e9aafc2e4ee565f10f
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-03-02 11:33:05 -0500

    Creates all GET functions

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,5 +1,5 @@
 // Shows
-
+const base = process.env.REACT_APP_API_BASE_URL;
 // Create
 export function createShow(show) {
   return;
@@ -11,22 +11,29 @@ export function destroyShow(id) {
 }
 
 // Index/Get all
-export function getAllShows() {
-  return;
+export async function getAllShows() {
+  const apiReponse = await fetch(`${base}/shows`);
+  const jsonResponse = await apiReponse.json();
+
+  return jsonResponse;
 }
 
 // Show/Get one
-export function getOneShow(id) {
-  return;
+export async function getOneShow(id) {
+  const apiReponse = await fetch(`${base}/shows/${id}`);
+  const jsonResponse = await apiReponse.json();
+
+  return jsonResponse;
 }
 
 // Update
-export function updateShow(id, show) {
-  return;
-}
+export async function updateShow(id, show) {}
 
 // Movies
 
-export function getAllMovies() {
-  return;
+export async function getAllMovies() {
+  const apiReponse = await fetch(`${base}/movies`);
+  const jsonResponse = await apiReponse.json();
+
+  return jsonResponse;
 }
