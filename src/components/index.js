5a9b8e242c2e68fbecde83df61a95f5fb8120eb8
HaroldF415 <harold.f415@gmail.com>
2023-03-05 18:44:33 -0500

    barrel exporting

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+// Barrel Exporting
+import Footer from "./common/Footer";
+import Home from "./home/Home";
+import Nav from "./common/Nav";
+import Show from "./shows/Show";
+import ShowsEditForm from "./shows/ShowsEditForm";
+import ShowsIndex from "./shows/ShowsIndex";
+import ShowsNewForm from "./shows/ShowsNewForm";
+
+export { Footer, Home, Nav, Show, ShowsEditForm, ShowsIndex, ShowsNewForm };
