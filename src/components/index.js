7ca317941381d25608c53d08fa20afa52f08601a
HaroldF415 <harold.f415@gmail.com>
2023-03-07 00:29:15 -0500

    MoviesIndex added to the barrel export

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,6 @@ import Show from "./shows/Show";
 import ShowsEditForm from "./shows/ShowsEditForm";
 import ShowsIndex from "./shows/ShowsIndex";
 import ShowsNewForm from "./shows/ShowsNewForm";
+import MoviesIndex from "./movies/MoviesIndex";
 
-export { Footer, Home, Nav, Show, ShowsEditForm, ShowsIndex, ShowsNewForm };
+export { Footer, Home, Nav, Show, ShowsEditForm, ShowsIndex, ShowsNewForm, MoviesIndex };
