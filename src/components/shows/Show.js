789efd8d9ec1e3027781a6e6b7dc4a89f7bffb82
HaroldF415 <harold.f415@gmail.com>
2023-03-07 01:17:55 -0500

    destroying a show functionality

--- src/components/shows/Show.js
+++ src/components/shows/Show.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 // useParams is a hook that allows us to access the URL parameters
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 import "./styles/Show.css";
 
@@ -20,6 +20,10 @@ function Show() {
   // The id parameter is passed in the URL from the ShowList component
   const { id } = useParams();
 
+  // useNavigate is a hook that allows us to navigate to a different route that we have defined in our app
+  // In this case we want to navigate to the showIndexPage after the user has deleted a show
+  const navigate = useNavigate();
+
   // useEffect is a hook that allows us to run code when the component mounts and when the component updates
   // The second argument is an array of dependencies.
   // If the array is empty, the code in the callback function will only run when the component mounts.
@@ -36,7 +40,18 @@ function Show() {
       });
   }, [id]);
 
-  const handleDelete = (showID) => {};
+  const handleDelete = (showID) => {
+    // when we delete a show, we want to remove it from the DOM and keep in mind the UX
+    // we want the user to navigate to the showIndexPage
+    destroyShow(showID)
+      .then(() => {
+        setShow({});
+        navigate("/shows", { state: { deletedShowTitle: show.title } });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <section className="shows-show-wrapper">
