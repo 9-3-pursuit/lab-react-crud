e92dc39ed53ee2661d8836afb21de20105a3e9fc
HaroldF415 <harold.f415@gmail.com>
2023-03-07 00:29:38 -0500

    trying to display movies

--- src/components/movies/MoviesIndex.js
+++ src/components/movies/MoviesIndex.js
@@ -1,3 +1,54 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getAllMovies } from "../../api/fetch";
+
+import ErrorMessage from "../errors/ErrorMessage";
+//import MovieListing from "./MovieListing";
+
+//import "./styles/MoviesIndex.css";
+
 export default function MoviesIndex() {
-  return <p>Movie List</p>;
+  const [error, setError] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+
+  const handleTextChange = (event) => {
+    const { value } = event.target;
+    setSearchTitle(value);
+  };
+
+  useEffect(() => {
+    getAllMovies()
+      .then((allMovies) => setMovies(allMovies))
+      .catch((catchError) => {
+        console.log(catchError);
+        setError(true);
+      });
+  }, []);
+
+  return (
+    <div>
+      {error ? (
+        <ErrorMessage />
+      ) : (
+        <section className="movies-index-wrapper">
+          <h2>All Movies</h2>
+          <button>
+            <Link to="/movies/new">Add a new show</Link>
+          </button>
+          <br />
+          <label htmlFor="searchTitle">
+            Search Movies:
+            <input type="text" value={searchTitle} id="searchTitle" onChange={handleTextChange} />
+          </label>
+          <section className="movies-index">
+            {movies.map((movie) => {
+              // return <MovieListing movie={movie} />;
+              console.log(movie);
+            })}
+          </section>
+        </section>
+      )}
+    </div>
+  );
 }
