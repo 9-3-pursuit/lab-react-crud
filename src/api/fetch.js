9426ee59f3e271e808a9d6b98f619aea520fb915
Jyoti Singh <jyo.singh88@gmail.com>
2023-03-31 18:08:09 -0400

    revisedErrors

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -3,11 +3,12 @@ const URL = process.env.REACT_APP_API_BASE_URL;
 // Create
 export function createShow(show) {
 const method = {
-  method : "POST"
+  method : "POST",
   body : JSON.stringify(show),
   headers : {"Content-Type": "application/json" },
 };
-return fetch(`${URL}/shows`,method).then((response) => response.json());
+const options = {method:"DELETE"}
+return fetch(`${URL}/shows/${id}`, options);
 }
 
 // Delete
@@ -18,7 +19,7 @@ export function destroyShow(id) {
 
 // Index/Get all
 export function getAllShows() {
-  return fetch (`${URL}/shows/${id}`).then((response) => response.json());
+  return fetch(`${URL}/shows`).then((response) => response.json());
 }
 
 // Show/Get one
@@ -39,31 +40,44 @@ export function updateShow(id, show) {
 }
 
 // Movies
-
 export function getAllMovies() {
-    return fetch(`${URL}/movies`).then((response) => response.json());
+  return fetch(`${URL}/movies`).then((response) => response.json());
 }
-export function getOneMovie(id) {
-  return fetch(`${URL}/movies/${id}`).then((response) => response.json());
-}
-export function destroyMovie(id) {
-  return fetch(`${URL}/movies/${id}`, { method: "DELETE" });
-}
+
+
+// Create
 export function createMovie(movie) {
-  const method = {
+  const options = {
     method: "POST",
     body: JSON.stringify(movie),
     headers: { "Content-Type": "application/json" },
   };
-  return fetch(`${URL}/movies`, method).then((response) => response.json());
+  return fetch(`${URL}/movies/`, options).then((response) => {
+    return response.json();
+  });
 }
+// Delete
+
+export function destroyMovie(id) {
+  const options = { method: "DELETE" };
+  return fetch(`${URL}/movies/${id}`, options);
+}
+
+
+// Movie/Get one
+export function getOneMovie(id) {
+  return fetch(`${URL}/movies/${id}`).then((response) => response.json());
+}
+
+
+// Update
 export function updateMovie(id, movie) {
-  const method = {
+  const options = {
     method: "PUT",
     body: JSON.stringify(movie),
     headers: { "Content-Type": "application/json" },
   };
-  return fetch(`${URL}/movies/${id}`, method).then((response) =>
-    response.json()
-  );
+  return fetch(`${URL}/movies/${id}`, options).then((response) => {
+    return response.json();
+  });
 }
